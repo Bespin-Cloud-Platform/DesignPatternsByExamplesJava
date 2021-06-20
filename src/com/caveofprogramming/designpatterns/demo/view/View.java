6b6cb75e763ac69f6a070e2282644d7d82b4bcc1
DANG Truong <dangtruong080593@gmail.com>
2021-06-20 10:08:28 +0700

    singleton pattern

--- src/com/caveofprogramming/designpatterns/demo/view/View.java
+++ src/com/caveofprogramming/designpatterns/demo/view/View.java
@@ -5,13 +5,12 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.*;
 
+import com.caveofprogramming.designpatterns.demo.model.Database;
 import com.caveofprogramming.designpatterns.demo.model.Model;
 
 public class View extends JFrame implements ActionListener {
@@ -20,6 +19,7 @@ public class View extends JFrame implements ActionListener {
     private JButton okButton;
     private JTextField nameField;
     private JPasswordField passField;
+    private JPasswordField repeatPassField;
 
     private LoginListener loginListener;
 
@@ -30,110 +30,125 @@ public class View extends JFrame implements ActionListener {
 
         nameField = new JTextField(10);
         passField = new JPasswordField(10);
-        okButton = new JButton("OK");
+        repeatPassField = new JPasswordField(10);
+        okButton = new JButton("Create user");
 
         setLayout(new GridBagLayout());
 
         GridBagConstraints gc = new GridBagConstraints();
         gc.anchor = GridBagConstraints.LAST_LINE_END;
-        gc.gridx=1;
-        gc.gridy=1;
-        gc.weightx=1;
-        gc.weighty=1;
+        gc.gridx = 1;
+        gc.gridy = 1;
+        gc.weightx = 1;
+        gc.weighty = 1;
         gc.insets = new Insets(100, 0, 0, 10);
-        gc.fill=GridBagConstraints.NONE;
+        gc.fill = GridBagConstraints.NONE;
 
         add(new JLabel("Name: "), gc);
 
         gc.anchor = GridBagConstraints.LAST_LINE_START;
-        gc.gridx=2;
-        gc.gridy=1;
-        gc.weightx=1;
-        gc.weighty=1;
+        gc.gridx = 2;
+        gc.gridy = 1;
+        gc.weightx = 1;
+        gc.weighty = 1;
         gc.insets = new Insets(100, 0, 0, 0);
-        gc.fill=GridBagConstraints.NONE;
+        gc.fill = GridBagConstraints.NONE;
 
         add(nameField, gc);
 
         gc.anchor = GridBagConstraints.LINE_END;
-        gc.gridx=1;
-        gc.gridy=2;
-        gc.weightx=1;
-        gc.weighty=1;
+        gc.gridx = 1;
+        gc.gridy = 2;
+        gc.weightx = 1;
+        gc.weighty = 1;
         gc.insets = new Insets(0, 0, 0, 10);
-        gc.fill=GridBagConstraints.NONE;
+        gc.fill = GridBagConstraints.NONE;
 
         add(new JLabel("Password: "), gc);
 
         gc.anchor = GridBagConstraints.LINE_START;
-        gc.gridx=2;
-        gc.gridy=2;
-        gc.weightx=1;
-        gc.weighty=1;
+        gc.gridx = 2;
+        gc.gridy = 2;
+        gc.weightx = 1;
+        gc.weighty = 1;
         gc.insets = new Insets(0, 0, 0, 0);
-        gc.fill=GridBagConstraints.NONE;
+        gc.fill = GridBagConstraints.NONE;
 
         add(passField, gc);
 
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.gridx = 1;
+        gc.gridy = 3;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.insets = new Insets(0, 0, 0, 10);
+        gc.fill = GridBagConstraints.NONE;
+
+        add(new JLabel("Repeat password: "), gc);
+
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.gridx = 2;
+        gc.gridy = 3;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.insets = new Insets(0, 0, 0, 0);
+        gc.fill = GridBagConstraints.NONE;
+
+        add(repeatPassField, gc);
+
         gc.anchor = GridBagConstraints.FIRST_LINE_START;
-        gc.gridx=2;
-        gc.gridy=3;
-        gc.weightx=1;
-        gc.weighty=100;
-        gc.fill=GridBagConstraints.NONE;
+        gc.gridx = 2;
+        gc.gridy = 4;
+        gc.weightx = 1;
+        gc.weighty = 100;
+        gc.fill = GridBagConstraints.NONE;
 
         add(okButton, gc);
 
         okButton.addActionListener(this);
+
+        // Database db = new Database();
+        // Database db = Database.getInstance();
+
+        addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowOpened(WindowEvent e) {
+                try {
+                    Database.getInstance().connect();
+                } catch (Exception e1) {
+                    JOptionPane.showMessageDialog(View.this, "Unable to connect to database.",
+                            "Error", JOptionPane.WARNING_MESSAGE);
+                    e1.printStackTrace();
+                }
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Database.getInstance().disconnect();
+            }
+
+        });
 
         setSize(600, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    public Model getModel() {
-        return model;
-    }
-
-    public void setModel(Model model) {
-        this.model = model;
-    }
-
-    public JButton getOkButton() {
-        return okButton;
-    }
-
-    public void setOkButton(JButton okButton) {
-        this.okButton = okButton;
-    }
-
-    public JTextField getNameField() {
-        return nameField;
-    }
-
-    public void setNameField(JTextField nameField) {
-        this.nameField = nameField;
-    }
-
-    public JPasswordField getPassField() {
-        return passField;
-    }
-
-    public void setPassField(JPasswordField passField) {
-        this.passField = passField;
-    }
-
-    public LoginListener getLoginListener() {
-        return loginListener;
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
 
         String password = new String(passField.getPassword());
-        String name = nameField.getText();
+        String repeat = new String(repeatPassField.getPassword());
 
-        fireLoginEvent(new LoginFormEvent(name, password));
+        if (password.equals(repeat)) {
+            String name = nameField.getText();
+
+            fireLoginEvent(new LoginFormEvent(name, password));
+        } else {
+            JOptionPane.showMessageDialog(this, "Passwords do not match.",
+                    "Error", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
     public void setLoginListener(LoginListener loginListener) {
@@ -141,7 +156,7 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void fireLoginEvent(LoginFormEvent event) {
-        if(loginListener != null) {
+        if (loginListener != null) {
             loginListener.loginPerformed(event);
         }
     }
